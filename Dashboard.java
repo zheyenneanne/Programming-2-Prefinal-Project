bde19d8414b1efd1e899322e467c10e98cdffd9c
xielynxie <rhexielyngalit@gmail.com>
2026-03-30 23:23:45 +0800

    Add files via upload

---
+++ Dashboard.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+
+public class Dashboard extends JFrame {
+
+    private JButton btnInvoice;
+    private JButton btnLogout;
+    private JPanel DashPanel;
+
+    public Dashboard() {
+        setTitle("Dashboard");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setContentPane(DashPanel);
+        setVisible(true);
+
+        btnInvoice.addActionListener(e -> {
+            new InvoiceGenerator().setVisible(true);
+        });
+
+        btnLogout.addActionListener(e -> {
+            new LoginForm();
+            dispose();
+        });
+    }
+}
