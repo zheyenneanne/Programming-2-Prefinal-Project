22a47b9faa870be751b50ab17382408b7a75b386
xielynxie <rhexielyngalit@gmail.com>
2026-03-31 20:14:53 +0800

    Update Dashboard.java

--- Dashboard.java
+++ Dashboard.java
@@ -7,11 +7,12 @@ public class Dashboard extends JFrame {
     private JPanel DashPanel;
 
     public Dashboard() {
-         setTitle("Dashboard");
-         setSize(400, 300);
+        setTitle("Dashboard");
+        setSize(400, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         setLocationRelativeTo(null);
+        setLocationRelativeTo(null);
         setContentPane(DashPanel);
+        setVisible(true);
         btnInvoice.addActionListener(e -> {
              new InvoiceGenerator().setVisible(true);
 
